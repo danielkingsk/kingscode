af098e1c8abc2d6111c4cafde394cef8edee8905
Daniel Kings <181596253+danielkingsk@users.noreply.github.com>
2025-05-17 21:12:09 -0400

    Updating Footer and Navbar sections

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,18 +1,14 @@
-import Image from 'next/image'
-import {footerLinks, socialMedia} from '../constants'
-import styles from "../styles"
-import logo from 'favicon.ico'
+import styles from "../style";
+import { logo } from "../assets";
+import { footerLinks, socialMedia } from "../constants";
 
-const Navbar = () => {
-  return (
-    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+const Footer = () => (
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
       <div className="flex-[1] flex flex-col justify-start mr-10">
-        <Image
-          src='/logo'
-          width={200}
-          height={200}
-          alt="kingscode"
+        <img
+          src={logo}
+          alt="hoobank"
           className="w-[266px] h-[72.14px] object-contain"
         />
         <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
@@ -48,25 +44,24 @@ const Navbar = () => {
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2022 Kings Code. All Rights Reserved.
+        Copyright Ⓒ 2022 HooBank. All Rights Reserved.
       </p>
 
-     {/* <div className="flex flex-row md:mt-0 mt-6">
-       //{socialMedia.map((social, index) => (
-          // <Image
-          //   key={social.id}
-          //   src={social.icon}
-          //   alt={social.id}
-          //   className={`w-[21px] h-[21px] object-contain cursor-pointer ${
-          //     index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
-          //   }`}
-          //   onClick={() => window.open(social.link)}
-          // />
-       // ))}
-      //</div>*/}
-    </div> 
-    </section>
-  )
-}
+      <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((social, index) => (
+          <img
+            key={social.id}
+            src={social.icon}
+            alt={social.id}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+            }`}
+            onClick={() => window.open(social.link)}
+          />
+        ))}
+      </div>
+    </div>
+  </section>
+);
 
-export default Navbar
+export default Footer;
